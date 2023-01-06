6a59d819ee9bab42e95ef8ed8797e7d341658e8e
shubh <shubhpachchigar@gmail.com>
2023-01-06 00:46:52 -0800

    nav-bar upgrade, token rerouting enabled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import AuthService from "./services/auth.service";
-import Button from '@mui/material/Button';
 import Register from "./components/register";
 import Profile from "./components/profile";
 import BoardUser from "./components/board-user";
@@ -16,6 +15,57 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import PeopleIcon from '@mui/icons-material/People';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Box from '@mui/material/Box';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import Typography from '@mui/material/Typography';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import Tooltip from '@mui/material/Tooltip';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '50ch',
+    },
+  },
+}));
 
 class App extends Component {
   constructor(props) {
@@ -56,78 +106,128 @@ class App extends Component {
 
   render() {
     const { currentUser } = this.state;
-
     return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <div className="navbar-nav mr-auto">
-
-            {currentUser && (
-              <li className="nav-item">
-                <Link to={"/friends"} className="nav-link">
-                  Friends
-                </Link>
-                
-              </li>
+      <Box sx={{ flexGrow: 1}}>
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            Greko
+          </Typography>
+          {currentUser && 
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search for friends…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search> }
+          <Box sx={{ flexGrow: 1 }} />
+          {!currentUser && <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Typography
+              variant="h7"
+              sx={{ display: { xs: 'none', sm: 'block' }, px: 1, color: 'white', "&:hover": { color: "#99CCFF !important" }  }}
+              component={Link}
+              to="/login"
+              style={{ textDecoration: 'none' }}
+            >
+                Login
               
-            )}
-          </div>
+            </Typography>
+            </Box>}
+            {!currentUser && <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Typography
+              variant="h7"
+              component={Link}
+              to="/register"
+              sx={{ display: { xs: 'none', sm: 'block' }, px: 1, color: 'white', "&:hover": { color: "#99CCFF !important" } }}
+              style={{ textDecoration: 'none' }}
+            >
+                Sign up
+            </Typography>
+          </Box>}
+          {currentUser && <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Tooltip title="Profile">
+          <IconButton size="large" color="inherit" component={Link} to="/user" sx={{ "&:hover": { color: "#FFF !important" } }}>
+              
+              <AccountBoxIcon />
 
-          {currentUser ? (
-            <div className="navbar-nav ml-auto">
+          </IconButton>
+          </Tooltip>
+          <Tooltip title="Friends">
+            <IconButton size="large" color="inherit" component={Link} to="/friends" sx={{ "&:hover": { color: "#FFF !important" } }}>
+              
+                <PeopleIcon />
 
-              <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                  Hello, {currentUser.username}
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                  <Button variant="contained" onClick={this.handleDialog}>Create thing</Button>
+            </IconButton>
+            </Tooltip>
+            <Tooltip title="Create thing">
+            <IconButton
+              size="large"
+              color="inherit"
+              onClick={this.handleDialog}
+            >
+             
+                <AddIcon />
+            </IconButton> 
+            
+            </Tooltip>
+            <CreateThing
+                    key={new Date().getTime()} 
+                    open={this.state.openDialog}
+                    OnClose={this.handleDialog}
+                />
+            {/* <Button variant="contained" onClick={this.handleDialog}><AddIcon /></Button>
                   <CreateThing
                       key={new Date().getTime()} 
                       open={this.state.openDialog}
                       OnClose={this.handleDialog} 
-                  />
-              </li>
-              
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
-                <Button variant="outlined">LogOut</Button>
-                  
-                </a>
-              </li>
-            </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Sign Up
-                </Link>
-              </li>
-            </div>
-          )}
-        </nav>
-
+                  /> */}
+            <Tooltip title="Logout">
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              color="inherit"
+              onClick={this.logOut}
+            >
+              <LogoutIcon />
+            </IconButton>
+            </Tooltip>
+          </Box>}
+        </Toolbar>
+        </AppBar>
         <div className="container mt-3">
           <Routes>
-            <Route path="/" element={<SignIn />} />
+            {!currentUser && <Route path="/" element={<SignIn />} /> }
             <Route path="/login" element={<SignIn />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/user" element={<BoardUser />} />
+            {currentUser && <Route path="/" element={<Calendar />} />}
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/friends" element={<Friends/>} />
           </Routes>
            
         </div> 
-      </div>
+      </Box>
+      
     );
   }
 }
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -15,7 +13,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import AuthService from "../services/auth.service";
 
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, Navigate} from 'react-router-dom';
 
 function Copyright(props) {
   return (
@@ -33,6 +31,7 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
+  const user = AuthService.getCurrentUser();
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -54,6 +53,10 @@ export default function SignIn() {
     );
   };
 
+  if(user){
+    return <Navigate to="/calendar" />;
+  }
+  else{
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -78,7 +81,7 @@ export default function SignIn() {
               required
               fullWidth
               id="email"
-              label="Email Address"
+              label="Username"
               name="email"
               autoComplete="email"
               autoFocus
@@ -119,4 +122,5 @@ export default function SignIn() {
       </Container>
     </ThemeProvider>
   );
+  }
 }
