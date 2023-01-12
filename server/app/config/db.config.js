64d96abc615d0e513b14425c8f0d644518814aee
shubh <shubhpachchigar@gmail.com>
2023-01-11 23:26:48 -0800

    transfer to azure cloud

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
+
+const HOST= process.env.DB_HOST;
+const USER= process.env.DB_USER;
+const PASSWORD= process.env.DB_PASSWORD;
+const DB= process.env.DB_NAME;
+
 module.exports = {
-    HOST: "34.125.181.146",
-    USER: "root",
-    PASSWORD: "khboTePYHqF3nK",
-    DB: "testdb",
+    HOST,
+    USER,
+    PASSWORD,
+    DB,
     dialect: "mysql",
     pool: {
       max: 5,
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://projectgreko.ml"
+  origin: process.env.PUBLIC_DOMAIN
 };
 
 app.use(cors(corsOptions));
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
